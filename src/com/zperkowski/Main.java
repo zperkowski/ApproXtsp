570bac014f3fce5143d8709f8e8250b2bc773237
zperkowski <ZdzislawPerkowski@outlook.com>
2017-04-18 15:17:05 +0200

    Created GUI

--- src/com/zperkowski/Main.java
+++ src/com/zperkowski/Main.java
@@ -12,7 +12,9 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         primaryStage.setTitle("ApproXtsp");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setMinHeight(425);
+        primaryStage.setMinWidth(600);
         primaryStage.show();
     }
 
