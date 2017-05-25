67186ed2c3b31ef4709cdf59a5867eec2672781d
zperkowski <ZdzislawPerkowski@outlook.com>
2017-05-25 22:58:22 +0200

    Probably working the Nearest Neighbour Algorithm

--- src/com/zperkowski/NearestNeighbourAlgorithm.java
+++ src/com/zperkowski/NearestNeighbourAlgorithm.java
@@ -6,9 +6,8 @@ import java.util.LinkedList;
  * Created by zperkowski on 24/05/2017.
  */
 public class NearestNeighbourAlgorithm {
-    private int currentVertex;
-    private double currentDistance;
-    private LinkedList<Integer> tourList;
+    private double distance;
+    private LinkedList<Integer> tourList = new LinkedList<Integer>();
     private TSPList tspList;
 
     public void addCities(TSPList cities) {
@@ -22,4 +21,50 @@ public class NearestNeighbourAlgorithm {
         tspList = null;
     }
 
+    public void calculateTour() {
+        if (tspList != null) {
+            TSPMatrix matrix = new TSPMatrix(tspList);
+            matrix.generateDistances();
+            double minCurrentDistance = matrix.get(0).get(1);
+            int currentVertex;
+            int closestCurrentNeighbour = 1;
+            tourList.add(0);
+            while (tourList.size() < matrix.size()) {
+                currentVertex = tourList.getLast();
+
+                // Looks for new value of minCurrentDistance
+                for (int i = matrix.size()-1; i > 0; i--)
+                    if (!tourList.contains(i) && currentVertex != i) {
+                        minCurrentDistance = matrix.get(currentVertex).get(i);
+                        closestCurrentNeighbour = i;
+                        break;
+                    }
+                // Looks for the true minCurrentDistance
+                for (int neighbour = 0; neighbour < matrix.size(); neighbour++) {
+                    if (!tourList.contains(neighbour) && currentVertex != neighbour)
+                        if (matrix.get(currentVertex).get(neighbour) < minCurrentDistance) {
+                            minCurrentDistance = matrix.get(currentVertex).get(neighbour);
+                            closestCurrentNeighbour = neighbour;
+                        }
+                }
+                tourList.add(closestCurrentNeighbour);
+                distance += minCurrentDistance;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        if (tourList.isEmpty())
+            return "";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < tourList.size(); i++) {
+            sb.append(tourList.get(i).toString() + " ");
+        }
+        sb.deleteCharAt(sb.length()-1);
+
+        return sb.toString();
+    }
 }
